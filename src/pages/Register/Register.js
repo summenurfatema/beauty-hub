41e672f714b93551096c96abe0e4ecf76596d9d4
Fatema04 <shadrinmoni15@gmail.com>
2023-01-12 23:39:29 +0600

    check box input field added

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -5,10 +5,15 @@ const Register = () => {
         <div className='flex flex-col justify-center items-center border-4'>
             <div className='w-[600px] border-4'>
                 <form className='px-5 py-7 space-y-5'>
-                    <input placeholder='Email' className='border-2 py-4 border-red w-full' />
-                    <input placeholder='Phone number' className='border-2 py-4 border-red w-full' />
-                    <input placeholder='Email' className='border-2 py-4 border-red w-full' />
-                    <input placeholder='Email' className='border-2 py-4 border-red w-full' />
+                    <input placeholder='Email' className='border-2 py-4 border-red w-full rounded-md' />
+                    <input placeholder='Phone number' className='border-2 py-4 border-red w-full rounded-md' />
+                    <input placeholder='Email' className='border-2 py-4 border-red w-full rounded-md' />
+                    <input placeholder='Email' className='border-2 py-4 border-red w-full rounded-md' />
+
+                    <div>
+                        <input type='checkbox' className='w-5 h-5' />
+                        <p className='inline-block pl-5 text-xl'> Accept all condition</p>
+                    </div>
                 </form>
             </div>
 
