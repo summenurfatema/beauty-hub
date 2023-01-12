9ba05569b4a9889ad051bcca949970ac565b7b03
Fatema04 <shadrinmoni15@gmail.com>
2023-01-12 23:31:21 +0600

    input field added

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -4,7 +4,7 @@ const Register = () => {
     return (
         <div className='flex flex-col justify-center items-center border-4'>
             <div className='w-[600px] border-4'>
-                <form className='px-5 py-7'>
+                <form className='px-5 py-7 space-y-5'>
                     <input placeholder='Email' className='border-2 py-4 border-red w-full' />
                     <input placeholder='Phone number' className='border-2 py-4 border-red w-full' />
                     <input placeholder='Email' className='border-2 py-4 border-red w-full' />
