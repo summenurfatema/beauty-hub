38561a8e02367bf34d0e727b9b00f5532c3919c9
Fatema04 <shadrinmoni15@gmail.com>
2023-01-13 10:39:38 +0600

    login edited

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
+        <div className='flex flex-col justify-center items-center bg-gray-100 py-10'>
+            <div className='w-[600px] bg-white pb-7'>
+                <h1 className='text-gray-800 text-center font-bold text-3xl py-7'>Create an account</h1>
+                <form className='px-8 py-7 space-y-5'>
+                    <input placeholder='Full name' className='border-2 py-4 border-red w-full rounded-md text-xl pl-5' />
 
-        </div>
-    );
+            </div>
+            );
 };
 
-export default Login;
+            export default Login;
