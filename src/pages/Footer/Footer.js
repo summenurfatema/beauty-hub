b0552df3f39a9cf47220126193da8be855cb2ddb
Fatema04 <shadrinmoni15@gmail.com>
2023-01-14 08:02:42 +0600

    footer 60% completed

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -4,15 +4,15 @@ const Footer = () => {
     return (
 
         <div
-            class="grid grid-cols-1 gap-5 pt-8 border-t border-gray-100 base:grid-cols-2 lg:grid-cols-4 lg:pt-16 bg-black pl-6"
+            class="grid grid-cols-1 gap-5 pt-8 border-t border-gray-100 base:grid-cols-2 lg:grid-cols-4 lg:pt-16 bg-black pl-6 pb-3"
         >
             <div classNam=''>
-                <p class="font-medium text-brown">ABOUT US</p>
+                <p class="font-medium text-lg text-brown">ABOUT US</p>
 
                 <nav aria-label="Footer Navigation - Services" class="mt-6">
                     <ul class="space-y-4 text-base">
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 About Beauty Hub
                             </a>
                         </li>
@@ -24,25 +24,25 @@ const Footer = () => {
                         </li>
 
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Skin Care Blog
                             </a>
                         </li>
 
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Our Location
                             </a>
                         </li>
 
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Contact Us
                             </a>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Submit Your Complain
                             </a>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Contact Us
                             </a>
                         </li>
@@ -51,29 +51,29 @@ const Footer = () => {
             </div>
 
             <div>
-                <p class="font-medium text-brown">MY ACCOUNT</p>
+                <p class="font-medium text-lg text-brown">MY ACCOUNT</p>
 
                 <nav aria-label="Footer Navigation - Company" class="mt-6">
                     <ul class="space-y-4 text-base">
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Login
                             </a>
                         </li>
 
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Order History
                             </a>
                         </li>
 
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Track Order
                             </a>
                         </li>
                         <li>
-                            <a href=" " class="text-gray-700 transition hover:opacity-75">
+                            <a href=" " class="text-gray-100 transition hover:opacity-75">
                                 Earn Money
                             </a>
                         </li>
@@ -82,78 +82,78 @@ const Footer = () => {
             </div>
 
             <div>
-                <p class="font-medium text-gray-900">FIND OUR BRANCH LOCATION</p>
+                <p class="font-medium text-lg text-brown">FIND OUR BRANCH LOCATION</p>
 
                 <nav aria-label="Footer Navigation - Company" class="mt-6">
                     <div className='flex space-x-4'>
                         <ul class="space-y-4 text-base">
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     DHANMONDI
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     MIRPUR
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     UTTARA
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     SHANTINAGAR
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     BASHUNDHARA CITY
                                 </a>
                             </li>
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     GAZIPUR
                                 </a>
                             </li>
                         </ul>
                         <ul class="space-y-4 text-base">
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     NARAYNGANJ
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     SYLHET
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     RAJSHAHI
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     CHATTOGRAM
                                 </a>
                             </li>
 
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     WARI
                                 </a>
                             </li>
                             <li>
-                                <a href=" " class="text-gray-700 transition hover:opacity-75">
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
                                     BANANI
                                 </a>
                             </li>
@@ -163,13 +163,13 @@ const Footer = () => {
             </div>
 
             <div>
-                <p class="font-medium text-gray-900">Corporate Office:</p>
+                <p class="font-medium text-xl text-brown">Corporate Office:</p>
 
                 <nav aria-label="Footer Navigation - Legal" class="mt-6">
                     <div>
-                        <p>Level 4, Mirpur DOHS Cultural Center</p>
-                        <p>Road No-09, Mirpur DOHS,</p>
-                        <p>Road No-09, Mirpur DOHS,</p>
+                        <p className='text-gray-100 text-base transition hover:opacity-75'>Level 4, Mirpur DOHS Cultural Center</p>
+                        <p className='text-gray-100 text-base transition hover:opacity-75'>Road No-09, Mirpur DOHS,</p>
+                        <p className='text-gray-100 text-base transition hover:opacity-75'>Mirpur 12, Dhaka-1216</p>
                         <div className='flex justify-around'>
                             <button className='bg-red-500 px-2 py-3'>CALL US</button>
                             <button className='bg-red-500 px-2 py-3'>EMAIL US</button>
