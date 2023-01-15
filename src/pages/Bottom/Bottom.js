bfb165014fabe486768aa2cd428424e55498ed58
Fatema04 <shadrinmoni15@gmail.com>
2023-01-15 22:36:12 +0600

    footer 10% done

--- src/pages/Bottom/Bottom.js
+++ src/pages/Bottom/Bottom.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Bottom = () => {
     return (
         <div>
+            <div className='flex justify-between items-center'>
+                <div>
+                    <p>Copyright 2023 © Bio-Xin Cosmeceuticals</p>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
 
         </div>
     );
