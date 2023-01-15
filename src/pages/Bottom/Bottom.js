103507051880ccbc402ebae58dc4c728506be5bf
Fatema04 <shadrinmoni15@gmail.com>
2023-01-15 23:23:55 +0600

    footer 40% done

--- src/pages/Bottom/Bottom.js
+++ src/pages/Bottom/Bottom.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { FaFacebook, FaInstagram, FaYoutube, FaCcMastercard } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaCcMastercard } from 'react-icons/fa';
+import { AiOutlineYoutube } from 'react-icons/ai'
 
 const Bottom = () => {
     return (
-        <div>
+        <div className=' px-9 py-7 bg-gray-800'>
             <div className='flex justify-between items-center'>
                 <div>
                     <p>Copyright 2023 © Bio-Xin Cosmeceuticals</p>
                 </div>
 
                 <div className='flex space-x-2'>
-                    <FaFacebook />
-                    <FaInstagram />
-                    <FaYoutube />
+                    <span className='h-10 w-10 bg-blue-700 flex justify-center items-center rounded-full'>
+                        <FaFacebookF className='text-2xl text-white' />
+                    </span>
+
+                    <span className='h-10 w-10 bg-pink-500 flex justify-center items-center rounded-full'>
+                        <FaInstagram className='text-2xl text-white' />
+                    </span>
+                    <span className='h-10 w-10 bg-red-700 flex justify-center items-center rounded-full'>
+                        <AiOutlineYoutube className='text-2xl text-white' />
+                    </span>
+
+
+
+
                 </div>
                 <div>
-                    <FaCcMastercard />
+                    <img src='assets/payment.png' alt='' />
                 </div>
             </div>
 
--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,197 +1,200 @@
 import React from 'react';
 import { GoPlus } from 'react-icons/go'
+import Bottom from '../Bottom/Bottom';
 
 const Footer = () => {
     return (
+        <div>
 
-        <div
-            class="grid grid-cols-1 gap-2 pt-8 border-t border-gray-100 base:grid-cols-2 lg:grid-cols-4 lg:pt-16 bg-black pl-6 pb-4"
-        >
-            <div classNam=''>
-                <p class="font-medium text-lg text-brown">ABOUT US</p>
+            <div
+                class="grid grid-cols-1 gap-2 pt-8 border-t border-gray-100 base:grid-cols-2 lg:grid-cols-4 lg:pt-16 bg-black pl-6 pb-4"
+            >
+                <div classNam=''>
+                    <p class="font-medium text-lg text-brown">ABOUT US</p>
 
-                <nav aria-label="Footer Navigation - Services" class="mt-6">
-                    <ul class="space-y-4 text-base">
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                About Beauty Hub
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                News & Events
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Skin Care Blog
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Our Location
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Contact Us
-                            </a>
-
-                        </li>
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Submit Your Complain
-                            </a>
-                        </li>
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Contact Us
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-
-            <div>
-                <p class="font-medium text-lg text-brown">MY ACCOUNT</p>
-
-                <nav aria-label="Footer Navigation - Company" class="mt-6">
-                    <ul class="space-y-4 text-base">
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Login
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Order History
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Track Order
-                            </a>
-                        </li>
-                        <li>
-                            <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                Earn Money
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-
-            <div>
-                <p class="font-medium text-lg text-brown">FIND OUR BRANCH LOCATION</p>
-
-                <nav aria-label="Footer Navigation - Company" class="mt-6">
-                    <div className='flex space-x-4'>
+                    <nav aria-label="Footer Navigation - Services" class="mt-6">
                         <ul class="space-y-4 text-base">
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' /> DHANMONDI
+                                    About Beauty Hub
                                 </a>
                             </li>
 
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' />   MIRPUR
+                                    News & Events
                                 </a>
                             </li>
 
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' />     UTTARA
+                                    Skin Care Blog
                                 </a>
                             </li>
 
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' />  SHANTINAGAR
+                                    Our Location
                                 </a>
                             </li>
 
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' /> BASHUNDHARA CITY
+                                    Contact Us
+                                </a>
+
+                            </li>
+                            <li>
+                                <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                    Submit Your Complain
                                 </a>
                             </li>
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' /> GAZIPUR
+                                    Contact Us
                                 </a>
                             </li>
                         </ul>
+                    </nav>
+                </div>
+
+                <div>
+                    <p class="font-medium text-lg text-brown">MY ACCOUNT</p>
+
+                    <nav aria-label="Footer Navigation - Company" class="mt-6">
                         <ul class="space-y-4 text-base">
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' /> NARAYNGANJ
+                                    Login
                                 </a>
                             </li>
 
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' />  SYLHET
+                                    Order History
                                 </a>
                             </li>
 
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' /> RAJSHAHI
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' /> CHATTOGRAM
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' />    WARI
+                                    Track Order
                                 </a>
                             </li>
                             <li>
                                 <a href=" " class="text-gray-100 transition hover:opacity-75">
-                                    <GoPlus className='inline-block font-bold' />  BANANI
+                                    Earn Money
                                 </a>
                             </li>
                         </ul>
-                    </div>
-                </nav>
-            </div>
+                    </nav>
+                </div>
 
-            <div>
+                <div>
+                    <p class="font-medium text-lg text-brown">FIND OUR BRANCH LOCATION</p>
 
-                <p class="font-medium text-xl text-brown">Corporate Office:</p>
+                    <nav aria-label="Footer Navigation - Company" class="mt-6">
+                        <div className='flex space-x-4'>
+                            <ul class="space-y-4 text-base">
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' /> DHANMONDI
+                                    </a>
+                                </li>
 
-                <nav aria-label="Footer Navigation - Legal" class="mt-6">
-                    <div>
-                        <p className='text-gray-100 text-base transition hover:opacity-75'>Level 4, Mirpur DOHS Cultural Center</p>
-                        <p className='text-gray-100 text-base transition hover:opacity-75'>Road No-09, Mirpur DOHS,</p>
-                        <p className='text-gray-100 text-base transition hover:opacity-75'>Mirpur 12, Dhaka-1216</p>
-                        <div className='flex space-x-4 py-3'>
-                            <button className='bg-gray-100 rounded-md px-3 py-2'>CALL US</button>
-                            <button className='bg-gray-100 rounded-md px-3 py-2'>EMAIL US</button>
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' />   MIRPUR
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' />     UTTARA
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' />  SHANTINAGAR
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' /> BASHUNDHARA CITY
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' /> GAZIPUR
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul class="space-y-4 text-base">
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' /> NARAYNGANJ
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' />  SYLHET
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' /> RAJSHAHI
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' /> CHATTOGRAM
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' />    WARI
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href=" " class="text-gray-100 transition hover:opacity-75">
+                                        <GoPlus className='inline-block font-bold' />  BANANI
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
+                <div>
+
+                    <p class="font-medium text-xl text-brown">Corporate Office:</p>
+
+                    <nav aria-label="Footer Navigation - Legal" class="mt-6">
+                        <div>
+                            <p className='text-gray-100 text-base transition hover:opacity-75'>Level 4, Mirpur DOHS Cultural Center</p>
+                            <p className='text-gray-100 text-base transition hover:opacity-75'>Road No-09, Mirpur DOHS,</p>
+                            <p className='text-gray-100 text-base transition hover:opacity-75'>Mirpur 12, Dhaka-1216</p>
+                            <div className='flex space-x-4 py-3'>
+                                <button className='bg-gray-100 rounded-md px-3 py-2'>CALL US</button>
+                                <button className='bg-gray-100 rounded-md px-3 py-2'>EMAIL US</button>
+
+                            </div>
+
+                            <button className='bg-brown text-white rounded-md px-3 py-2 mb-2'>Appoinment</button>
+                            <div> <img className='rounded-md h-12 w-32 mb-2 border-2' src='assets/gp2.png' alt=' ' />
+                            </div>
 
                         </div>
-
-                        <button className='bg-brown text-white rounded-md px-3 py-2 mb-2'>Appoinment</button>
-                        <div> <img className='rounded-md h-12 w-32 mb-2 border-2' src='assets/gp2.png' alt=' ' />
-                        </div>
-
-                    </div>
-                </nav>
+                    </nav>
+                </div>
             </div>
 
-
+            <Bottom />
 
 
 
