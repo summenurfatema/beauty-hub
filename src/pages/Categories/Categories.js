899742ca424450e509eec1e56f472ff9bbb69de5
Fatema04 <shadrinmoni15@gmail.com>
2023-01-24 12:18:45 +0600

    category added in json file

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -7,49 +7,51 @@ const Categories = () => {
             'id': 1,
             'name': 'WHITENING & BRIGHTENING',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/3gH32kgMh3z38UVoxrfFScyKX7O33qTJhzXK7G40.webp',
-            "category": "whitening"
+            "category": "whitening-category"
         },
         {
             'id': 2,
             'name': 'OILY & ACNE SKIN CARE',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/dGT0AD0grJogwOOIAHhbzshzJDqEgLqm5sdK0ilB.webp',
-            "category": "oily"
+            "category": "oily-category"
         },
+
+
         {
             'id': 3,
             'name': 'EXCLUSIVE SKIN CARE',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/tXxESuckqM6HNX2VAhj5ZwMBooOOR4MtN4O7GBEQ.webp',
-            "category": "exclusive"
+            "category": "exclusive-category"
         },
         {
             'id': 4,
             'name': 'MOISTURIZER',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/3gH32kgMh3z38UVoxrfFScyKX7O33qTJhzXK7G40.webp',
-            "category": "moisturizer"
+            "category": "moisturizer-category"
         },
         {
             'id': 5,
             'name': 'PREMIUM SKINCLINIC PRODUCTS',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/yr0xgl0ESXaSxiwvkclFpMUEQw4tKs1v1FniIdRM.webp',
-            "category": "premium"
+            "category": "premium-category"
         },
         {
             'id': 6,
             'name': 'DAILY SKIN CARE',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/yr0xgl0ESXaSxiwvkclFpMUEQw4tKs1v1FniIdRM.webp',
-            "category": "daily"
+            "category": "daily-category"
         },
         {
             'id': 7,
             'name': 'MATURE SKIN CARE',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/zTf8m4SUDARWOfAntjQpcnrSlnkZL2HrHU0bFzkq.webp',
-            "category": "mature"
+            "category": "mature-category"
         },
         {
             'id': 8,
             'name': 'HYPERPIGMENTATION, SPOT & SCAR',
             'img': 'https://bioxin.s3.ap-south-1.amazonaws.com/uploads/all/pZ5fRkyL85ll9oZQjGnTdONkbmk1xBkplOxVCZhP.webp',
-            "category": "spot"
+            "category": "spot-category"
         }
     ]
     return (
